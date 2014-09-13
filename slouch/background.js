c674764e0131c08e147f2798c88a096e0380fca1
Vinnie Magro <vmagro@socaldevs.com>
2014-09-13 04:51:31 -0400

    Serial working a little bit

--- slouch/background.js
+++ slouch/background.js
@@ -1,8 +1,36 @@
+function onReceive(connectionId, data){
+  var contentWindow = chrome.app.window.getAll()[0].contentWindow;
+  contentWindow.document.body.innerHTML += data;
+}
+
 chrome.app.runtime.onLaunched.addListener(function() {
+  console.log('launched');
+
   chrome.app.window.create('window.html', {
     'bounds': {
       'width': 400,
       'height': 500
     }
   });
+
+  chrome.serial.getDevices(function(ports) {
+    var port = null;
+
+    for(var i = 0; i < ports.length; i++) {
+      console.log(ports[i].path);
+      var path = ports[i].path;
+      console.log(path);
+      if(path.indexOf('/tty.usbserial')) {
+        port = ports[i].path;
+      }
+    }
+
+    chrome.serial.connect(port, function(connectionInfo){
+      console.log('connected!');
+      onReceive(0, "hello");
+
+      chrome.serial.onReceive.addListener(onReceive)
+    });
+  });
+
 });
