4960bae24a1112e06204eb7769f193cc56b2341a
Peter Zhang <p@peetahzee.com>
2014-09-13 05:12:31 -0400

    CHANGE TO VEIL

--- fitfactor/background.js
+++ fitfactor/background.js
@@ -1,8 +1,6 @@
 chrome.runtime.onConnectExternal.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
-  	console.log(msg);
   	chrome.tabs.query({url: 'https://www.facebook.com/*'}, function(tabs) {
-  		console.log(tabs);
   		tabs.forEach(function(tab) {
   			chrome.tabs.sendMessage(tab.id, msg);	
   		});
--- fitfactor/content_script.js
+++ fitfactor/content_script.js
@@ -2,17 +2,17 @@ var blockDiv = null;
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	if(request.off) {
-  		if (blockDiv) {
-  			document.getElementsByTagName('body')[0].removeChild(blockDiv);
-  			blockDiv = null;
-  		}
-	 	} else if (request.on) {
-	 		if (!blockDiv) {
+  	if(request.veil) {
+  		if (!blockDiv) {
 		 		blockDiv = document.createElement('div');
 	  		blockDiv.setAttribute('style', 'width:100%;height:100%;background:#000;position:absolute;top:0;left:0;z-index:9999');
 	  		document.getElementsByTagName('body')[0].appendChild(blockDiv);
 	  	}
+	 	} else {
+	 		if (blockDiv) {
+  			document.getElementsByTagName('body')[0].removeChild(blockDiv);
+  			blockDiv = null;
+  		}
   	}
   }
 );
