116ad6c841e7d8fb3b15e3b6f25262994953375a
Vinnie Magro <vmagro@socaldevs.com>
2014-09-13 16:46:58 -0400

    Filter steps only within interval

--- fitfactor/background.js
+++ fitfactor/background.js
@@ -1,5 +1,10 @@
 var user = null;
 
+var stepsGoal = 10;
+//20 hours in ms
+//var goalInterval = 20 * 60 * 60 * 1000;
+var goalInterval = 60 * 1000;
+
 function init(){
   Parse.initialize('KqI3fIwrmgp1rep6UX31wZipcJACRJwtG66GNYoV', 'Xc5WRZyHmPAA0VeCJjKOcHJi8avThuzwlOQOX2pP');
 
@@ -18,15 +23,6 @@ chrome.runtime.onStartup.addListener(init);
 chrome.runtime.onConnectExternal.addListener(function(port) {
   port.onMessage.addListener(send);
 });
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log('received request', request);
-    console.log(sender);
-    chrome.tabs.sendMessage(sender.tab.id, {veil: true});
-  }
-);
-
 
 function notify(userId, forUserId){
   var notifyUser = new Parse.User();
@@ -52,26 +48,59 @@ function notify(userId, forUserId){
   });
 }
 
-function checkParse(){
+function checkParse(callback){
+  getSteps(function(steps) {
+    isFriendUnlock(function(friendUnlock) {
+      var message = {
+        slouch: false,
+        steps: steps > stepsGoal,
+        stepsCount: steps,
+        stepsGoal: {
+          count: stepsGoal,
+          interval: goalInterval
+        },
+        friendUnlock: friendUnlock
+      };
+
+      send(message);
+    });
+  })
+}
+
+function getSteps(callback) {
   var StepCountUpdate = Parse.Object.extend('StepCountUpdate');
   var recentSteps = new Parse.Query(StepCountUpdate);
   recentSteps.equalTo('user', user);
+  recentSteps.greaterThan('createdAt', new Date(new Date().getTime() - goalInterval));
 
+  recentSteps.find({
+    success: function(results) {
+      var totalSteps = 0;
+      // Do something with the returned Parse.Object values
+      for (var i = 0; i < results.length; i++) {
+        var object = results[i];
+        totalSteps += parseInt(object.get('value'));
+      }
+      callback(totalSteps);
+    },
+    error: function(error) {
+      console.error("Error: " + error.code + " " + error.message);
+    }
+  });
+}
+
+function isFriendUnlock(callback) {
   var FriendUnlock = Parse.Object.extend('FriendUnlock');
   var friendQuery = new Parse.Query(FriendUnlock);
   friendQuery.equalTo('unlockFor', user);
 
-  var query = Parse.Query.or(recentSteps, friendQuery);
-  query.find({
-    success: function(results) {
-      // Do something with the returned Parse.Object values
-      for (var i = 0; i < results.length; i++) {
-        var object = results[i];
-        //alert(object.id + ' - ' + object.get('value'));
-      }
+  friendQuery.count({
+    success: function(count) {
+      callback(count > 0);
     },
     error: function(error) {
-      console.error("Error: " + error.code + " " + error.message);
+      // The request failed
+      callback(false);
     }
   });
 }
