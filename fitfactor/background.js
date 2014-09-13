b76894c05d3d0d0dc74c0391802cbb6f0ee1857c
Vinnie Magro <vmagro@socaldevs.com>
2014-09-13 13:06:36 -0400

    Think I added parse correctly this time

--- fitfactor/background.js
+++ fitfactor/background.js
@@ -1,11 +1,15 @@
+chrome.runtime.onStartup.addListener(function(){
+  Parse.initialize("kKHah32xHCgNU7we80esxavR7rlecBjMV0sNtYjt", "TnxvmngkwSx7xGsWq29Y6W4acVXFp9uC093b3CjT");
+});
+
 chrome.runtime.onConnectExternal.addListener(function(port) {
   port.onMessage.addListener(function(msg) {
   	console.log(msg);
   	chrome.tabs.query({url: 'https://www.facebook.com/*'}, function(tabs) {
   		tabs.forEach(function(tab) {
-  			chrome.tabs.sendMessage(tab.id, msg);	
+  			chrome.tabs.sendMessage(tab.id, msg);
   		});
-		  
+
 		});
 
   });
